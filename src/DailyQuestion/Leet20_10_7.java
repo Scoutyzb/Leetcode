21949b9bb7bfcfde7c232082f4898a3215e5149b
Scout <32534891+Scoutyzb@users.noreply.github.com>
2020-10-13 22:44:27 +0800

    Update Leet20_10_7.java

--- src/DailyQuestion/Leet20_10_7.java
+++ src/DailyQuestion/Leet20_10_7.java
@@ -1,2 +1,21 @@
-package DailyQuestion;public class Leet20_10_7 {
+package DailyQuestion;
+
+public class Leet_10_7 {
+    public void sortColors(int[] nums) {
+        int p0 = 0;
+        int p2 = nums.length - 1;
+        for (int i = 0; i < nums.length&&i<p2; i++) {
+            if (nums[i] == 0)
+                swap(nums, i, p0++);
+            else if(nums[i] == 2)
+                swap(nums, i-- , p2--);
+        }
+    }
+        private void swap(int[] nums,int i ,int j){
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+
 }
