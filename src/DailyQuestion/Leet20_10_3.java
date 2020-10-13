20120dd9e91ef2bfd2a9c435af48f50a309c2a77
Scout <32534891+Scoutyzb@users.noreply.github.com>
2020-10-13 22:42:58 +0800

    Update Leet20_10_3.java

--- src/DailyQuestion/Leet20_10_3.java
+++ src/DailyQuestion/Leet20_10_3.java
@@ -1,2 +1,28 @@
-package DailyQuestion;public class Leet20_10_3 {
+package DailyQuestion;
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+
+public class Leet_10_3 {
+    public int[] twoSum_doublcircle(int[] nums, int target) {
+        int i=0;
+        int j=0;
+        for( i = 0; i < nums.length;i++)
+            for( j = i+1 ; j < nums.length;j++)
+                if(nums[i]+nums[j]==target)
+                    return new int[]{i,j};
+        return new int[]{i,j};
+    }
+    public int[] twoSum_hash(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for(int i = 0 ; i < nums.length ; i++){
+            if(map.containsKey(target-nums[i]))
+                return (new int[]{i,map.get(target-nums[i])});
+            else
+                map.put(nums[i],i);
+        }
+        return null;
+    }
 }
