9398843ae3a28a357652d9ea06b02e0d559e3106
Scout <32534891+Scoutyzb@users.noreply.github.com>
2020-10-13 22:44:47 +0800

    Update Leet20_10_8.java

--- src/DailyQuestion/Leet20_10_8.java
+++ src/DailyQuestion/Leet20_10_8.java
@@ -1,2 +1,16 @@
-package DailyQuestion;public class Leet20_10_8 {
+package DailyQuestion;
+
+import java.util.HashMap;
+
+public class Leet_10_8 {
+    public void reverseString(char[] s) {
+        int i=0;
+        int j=s.length-1;
+        while(i<j){
+            char temp = s[i];
+            s[i++] = s[j];
+            s[j--] = temp;
+        }
+        HashMap map = new HashMap();
+    }
 }
